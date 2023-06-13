6aea8703ce4fcca08ad2df972333ba6b8c4adcaa
TheNolle <github@thenolle.com>
2023-06-13 19:45:34 +0200

    First Step into Typescript Bot Development !

---
+++ index.ts
@@ -0,0 +1,48 @@
+import { Client, GatewayIntentBits, Partials } from 'discord.js'
+import config from './config.json'
+
+const intents = [
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent
+]
+
+const partials = [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.GuildScheduledEvent,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.ThreadMember,
+    Partials.User
+]
+
+const client = new Client({ intents, partials })
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`)
+})
+
+client.on('messageCreate', message => {
+    if (message.content.toLowerCase() === `hello!`) {
+        message.channel.send('Hi!')
+    }
+})
+
+client.login(config.bot.token)
